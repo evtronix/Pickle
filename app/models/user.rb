8efcc595bccb95f2ad33baf039e8ddc6699d6c12
Evan Dorfman <edorfs@gmail.com>
2012-10-16 18:28:16 -0400

    Add standings

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
@@ -18,5 +18,44 @@ class User < ActiveRecord::Base
 
   def total_games
     self.games.where('result != ?', "P").count 
+  end
+
+  def percentage_correct
+    ((self.correct_games / self.total_games) * 100).to_i
+  end
+
+  def self.sorted_by_percentage_correct
+    User.all.sort_by(&:percentage_correct).reverse
+  end
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
   end
 end
